157ec7fa314d47b1d588734862a40815cde2c530
Robert <95480383+robertiki2001@users.noreply.github.com>
2023-12-06 20:05:29 +0100

    Delete Ej1 directory

--- Ej1/script.ts
+++
@@ -1,9 +0,0 @@
-import { Persona } from "./personaClass";
-
-const persona1 = new Persona('Juan', 30, '12345678A', 'H', 70, 175);
-const persona2 = new Persona('María', 25, '98765432B', 'M', 55, 160);
-const persona3 = new Persona('Carlos', 40, '56789012C', undefined, 80, 180);
-
-console.log(persona1);
-console.log(persona2);
-console.log(persona3);
